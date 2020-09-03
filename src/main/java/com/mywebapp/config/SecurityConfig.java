4a2c42382d9783cc4c4dd110eac3352a74af8f96
amand <amandoz@list.ru>
2020-09-03 14:18:57 +0300

    first

---
+++ src/main/java/com/mywebapp/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.mywebapp.config;
+
+import com.mywebapp.config.handler.LoginSuccessHandler;
+import com.mywebapp.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder(12);
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/login")
+                .successHandler(new LoginSuccessHandler())
+                .loginProcessingUrl("/login");
+        http.logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .and()
+                .csrf()
+                .disable();
+        http.authorizeRequests()
+                .antMatchers("/login")
+                .anonymous()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/user").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+}
---
+++ src/main/java/com/mywebapp/controller/AdminController.java
@@ -0,0 +1,91 @@
+package com.mywebapp.controller;
+
+import com.mywebapp.model.Role;
+import com.mywebapp.model.User;
+import com.mywebapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private UserService userService;
+
+    public AdminController() {
+
+    }
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String listController(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "listuser";
+    }
+
+    @GetMapping("/createuser")
+    public String createUserController(Model model) {
+        model.addAttribute("user", new User());
+        return "createuser";
+    }
+
+    @PostMapping("/createuser")
+    public String createUserController(@ModelAttribute("user") User user,
+                                       @RequestParam(value = "userCheck", required = false) boolean userCheck,
+                                       @RequestParam(value = "adminCheck", required = false) boolean adminCheck) {
+        Set<Role> roles = new HashSet<>();
+        if (userCheck) {
+            roles.add(new Role(1, "ROLE_ADMIN"));
+        }
+        if (adminCheck) {
+            roles.add(new Role(2, "ROLE_USER"));
+        }
+        user.setRoles(roles);
+        userService.saveUser(user);
+
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUserController(@PathVariable long id) {
+        userService.deleteUserById(id);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUserController(@PathVariable long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", user.getRoles().toString());
+        return "edituser";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editUserController(@ModelAttribute("user") User user,
+                                     @RequestParam(value = "userCheck", required = false) boolean userCheck,
+                                     @RequestParam(value = "adminCheck", required = false) boolean adminCheck) {
+        user.setRoles(null);
+        Set<Role> roles = new HashSet<>();
+        if (adminCheck) {
+            roles.add(new Role(1, "ROLE_ADMIN"));
+        }
+        if (userCheck) {
+            roles.add(new Role(2, "ROLE_USER"));
+        }
+        user.setRoles(roles);
+        userService.updateUser(user);
+        return "redirect:/admin/";
+    }
+
+}
